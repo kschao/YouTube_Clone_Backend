a9941ad4f69053378331299c714a307c490a5e5d
Steve Clark <steve.a.clark48@gmail.com>
2021-08-20 10:33:48 -0500

    Created Navbar function

---
+++ youtube-clone-frontend/src/Components/NavBar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Menu, Segment } from 'semantic-ui-react';
+
+function NavBar () {
+    return (
+        <Segment inverted>
+            <Menu inverted secondary>
+                <Menu.Item
+                name='home'
+                />
+            </Menu>
+        </Segment>
+    )
+}
+
+export default NavBar;
