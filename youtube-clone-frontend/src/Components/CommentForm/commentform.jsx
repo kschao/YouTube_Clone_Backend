edd334ffc24ef7a23db57712b66cd29a867cb69a
Steve Clark <steve.a.clark48@gmail.com>
2021-08-20 10:21:16 -0500

    Created Comment Form Class

---
+++ youtube-clone-frontend/src/Components/CommentForm/commentform.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Form, TextArea, Button } from 'semantic-ui-react'
+
+class CommentForm extends Component {
+    const(props) {
+        super(props);
+        this.state = {
+            comments: '',
+            errors: {
+                comments: ''
+            }
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.handleSubmit}>
+                    <TextArea type ="text" name="comments" placeholder="150 characters" value={this.state.comments} onChange={this.handleChange} />
+                    <br></br>
+                    <Button color='orange' type="submit">Cancel</Button>
+                    <Button positive type="submit">Comment</Button>
+                </Form>
+            </div>
+        );
+    }
+}
+
+
+export default CommentForm;
