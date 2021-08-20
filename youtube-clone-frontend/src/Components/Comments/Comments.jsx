c33cdac1af1afee6f53d478d66656a2a09ead5cc
Steve Clark <steve.a.clark48@gmail.com>
2021-08-20 10:27:43 -0500

    Created Comments function

---
+++ youtube-clone-frontend/src/Components/Comments/Comments.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react'
+
+function Comments () {
+    return (
+        <Card fluid color='yellow' header='(0) Comments' />
+    )
+}
+
+export default Comments;
