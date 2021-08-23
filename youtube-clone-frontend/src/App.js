a1f351df1efc0188a0f93f138ca79fd94413decd
Steve Clark <steve.a.clark48@gmail.com>
2021-08-23 14:30:04 -0500

    made changes to functions

--- youtube-clone-frontend/src/App.js
+++ youtube-clone-frontend/src/App.js
@@ -1,67 +1,160 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import 'semantic-ui-css/semantic.min.css'
-import { Container, Divider, Icon } from 'semantic-ui-react'
-import NavBar from './Components/NavBar/navbar';
-import Comments from './Components/Comments/Comments'
-import CommentForm from './Components/CommentForm/commentform';
-import SearchBar from './Components/SearchBar/searchbar';
-import { apikey } from '../src/APIkey';
+import SearchBar from './components/SearchBar/searchBar';
+import CommentForm from './components/CommentForm/commentForm';
+import CommentList from './components/CommentList/commentList';
+import RelatedVideos from './components/RelatedVideos/relatedVideos'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      videoId: 'd0V2FdtFI_w',
-      selectedVideo: { id: { videoId: 'd0V2FdtFI_w'}},
-      videoTitle: '',
-      videoDescription: '',
-      relatedVideos: [],
-      RecommendedVideos: []
+      this.state = {
+        comments: [],
+        filteredComments: [],
+        replies: [],
+        videoId: '',
+        videoTitle: '',
+        videoDescription: '',
+        relatedVideos: [],
+      
+      }
+  }
+
+  componentDidMount() {
+    this.searchVideo('software development')
+    this.getComments();
+  }
+
+  searchVideo = async (searchQuery) => {
+    let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchQuery}&type=video&part=snippet&key=AIzaSyAIfh92bqWo0T_AbXjELe4jIF2iDLZvb18`);
+    let allVideos = response.data;
+
+    this.getRelatedVideos({
+      videoId: allVideos.items[0].id.videoId,
+      videoTitle: allVideos.items[0].snippet.title,
+      videoDescription: allVideos.items[0].snippet.description,
+    })
+
+  }
+
+  getRelatedVideos = async (videoData) => { 
+    let response = await axios.get (`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoData.videoId}&type=video&part=snippet&key=AIzaSyAIfh92bqWo0T_AbXjELe4jIF2iDLZvb18`);
+    let relatedVideos = response.data.items.filter(video => video.snippet);
+    let relatedVideosArray = relatedVideos.map((video) => {
+      return ({
+          videoId: video.id.videoId,
+          videoTitle: video.snippet.title,});
+      });
+      this.setState({
+        videoId: videoData.videoId,
+        videoTitle: videoData.videoTitle,
+        videoDescription: videoData.videoDescription,
+        relatedVideos: relatedVideosArray
+    })
+  }
+
+  getComments = async () => {
+    try{
+      console.log("get all comments request is called")   // test
+      let response = await axios.get('http://127.0.0.1:8000/comments/')
+          this.setState({
+          comments: response.data,
+          })
+      this.filterComments();
+    }
+    catch (err) {
+      console.log(err)
     }
   }
 
-
-  componentDidMount() {
+  filterComments = () => {
+    let filtered = this.state.comments.filter(comment => comment.video_id.includes(this.state.videoId))
+    console.log(this.state.videoId)   // test
+    this.setState({
+      filteredComments:filtered
+    })
+    console.log(this.state.filteredComments);
   }
 
-  handleSubmit = async (searchTerm) => {
-    debugger;
-		const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-			params: {
-				part: 'snippet',
-				maxResults: 5,
-				key: 'AIzaSyCjN-KiVubfVkVrOHCiY4ADEKeLU9bkqXk',
-				q: searchTerm
-			}
-		});
+  likeComment = async (id, video_id) => {
+    try{
+      await axios.patch(`http://127.0.0.1:8000/comments/${id}/${video_id}/1/`)
+      let response = await this.getComments()
+      if(response === undefined) {
+        this.setState({
+        })
+      }
+      else{
+        this.setState({
+          comments: response.data
+        });
+      }
+    }
+    catch(err) {
+      console.log(err);
+    }
+  }
 
-		this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
-	};
+  dislikeComment = async (id, video_id) => {
+    try{
+      await axios.patch(`http://127.0.0.1:8000/comments/${id}/${video_id}/2/`)
+      let response = await this.getComments()
+      if(response === undefined) {
+        this.setState({
+        })
+      }
+      else{
+        this.setState({
+          comments: response.data
+        });
+      }
+    }
+    catch(err) {
+      console.log(err);
+    }
+  }
 
-  render() {
-    console.log(apikey)
+  render() { 
     return (
-    <React.Fragment>
-    <div>
-      <Container>
-      <Divider />
-      <NavBar/>
-      <SearchBar onFormSubmit={this.handleSubmit} />
-      <h1>Youtube Clone <Icon name='video' size='small' /></h1>
-      <Divider />
-      <h1>{this.state.videoTitle}</h1>
-      <iframe className="iframe" title="title" id="ytplayer" type="text/html" width="640" height="360"
-        src={`https://www.youtube.com/embed/${this.state.selectedVideo.id.videoId}?autoplay=1&origin=http://example.com`}
-          frameBorder="0"><br/>
-      </iframe>  
-      <p>{this.state.videoDescription}</p>
-      <Comments/>
-      <CommentForm/>
-      </Container>
-    </div>
-    </React.Fragment>
-    )
+      <div className="bg-secondary ">
+        <React.Fragment>
+        <br />
+          <br />
+          <div className="container bg-dark text-light border border-primary">
+          <u><h1 className="marquee">YouTube Clone</h1></u>
+          <br />
+          <SearchBar searchVideo={this.searchVideo}/>
+          </div>
+          <br />
+          <br />
+          <br />
+          <div className="d-flex justify-content-center">
+          <iframe class="border border-primary" id="ytplayer" title="title" type="text/html" width="640" height="360"
+              src={`https://www.youtube.com/embed/${this.state.videoId}?`}
+              frameborder="0"></iframe>
+          </div>
+          <div className="container">
+          <h2>{this.state.videoTitle}</h2>
+          <h3>{this.state.videoDescription}</h3>
+          </div>
+          <br />
+          <br />
+          <div className="container bg-dark text-light border border-primary">
+          <RelatedVideos relatedVideos={this.state.relatedVideos} />
+          </div>
+          <br />
+          <br />
+          <div className="container bg-dark text-light border border-primary">
+          <CommentForm getComments={this.getComments} videoId={this.state.videoId}/>
+          <br />
+          <br />
+          <CommentList filteredComments={this.state.filteredComments}
+          likeComment={this.likeComment} dislikeComment={this.dislikeComment}
+          showReplies={this.state.replies} />
+          </div> 
+        </React.Fragment>
+      </div>
+    );
   }
 }
 
